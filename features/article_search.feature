6bdbc115d63c3c70721a70fd031698d30e22f134
TERRANEO Federico <federico.terraneo@soprasteria.com>
2019-06-25 15:21:11 +0200

    added parameters in scenario

--- features/article_search.feature
+++ features/article_search.feature
@@ -2,7 +2,11 @@ Feature: Article Search
 
    The user want to search an article
 
-    Scenario: Finding a Playstation 4
+    Scenario Outline: Finding a  Bike
         Given I am on the search page of unieuro
-        When I search for "PS4"
-        Then at least one element of the first 10 item of the list should have the caption "Playstation 4"
+        When I search for "<term>"
+        Then at least one element of the first 10 item of the list should have the caption "<caption>"
+
+    Examples:
+    | term | caption |
+    | bici  |  Bicicletta  |
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -8,9 +8,10 @@ async function autoUIGetListElementsCaptions(searchTerm) {
         var elementCaptions = [];
 
         let driver = new webdriver.Builder().forBrowser('chrome').build();
-        await driver.get('https://www.unieuro.it/online/');
+        await driver.get('https://onlinestore.eurospin.it');
 
-        let element = await driver.findElement(webdriver.By.id('algolia-desktop-search-input'));
+        //let element = await driver.findElement(webdriver.By.id('algolia-desktop-search-input'));
+        let element = await driver.findElement(webdriver.By.css('#search'));
         await element.sendKeys(searchTerm, webdriver.Key.ENTER);
 
         var articles;
@@ -21,10 +22,10 @@ async function autoUIGetListElementsCaptions(searchTerm) {
 
         //     driver.quit();
         // });
-        await driver.wait(webdriver.until.elementLocated(webdriver.By.css("#instant-results > div.listing-container > main > div.items-container > section > section:nth-child(1) > article")),10000);
-
-        articles = await driver.findElements(webdriver.By.css("#instant-results > div.listing-container > main > div.items-container > section > section > article > div.item-container > div.info > div.category"));
-
+        //await driver.wait(webdriver.until.elementLocated(webdriver.By.css("#instant-results > div.listing-container > main > div.items-container > section > section:nth-child(1) > article")),10000);
+        await driver.wait(webdriver.until.elementLocated(webdriver.By.css("#maincontent > div.columns > div > div.search.results > div.products.wrapper.grid.products-grid > ol > li:nth-child(1)")),10000);
+        //articles = await driver.findElements(webdriver.By.css("#instant-results > div.listing-container > main > div.items-container > section > section > article > div.item-container > div.info > div.category"));
+        articles = await driver.findElements(webdriver.By.css("#maincontent > div.columns > div > div.search.results > div.products.wrapper.grid.products-grid > ol > li:nth-child(1) > div > div.product.details.product-item-details > strong > a"));
         //console.log('length: ' + articles.length);
 
         let base64 = await driver.takeScreenshot();
@@ -48,15 +49,18 @@ Given('I am on the search page of unieuro', function() {
 
 })
 
-When('I search for "PS4"', {timeout: 6 * 5000}, async function () {
-    this.actualResults = await autoUIGetListElementsCaptions("PS4");
-    console.log("actualResults=" + this.actualResults.length);
+When('I search for {string}', {timeout: 8 * 5000}, async function (term) {
+    this.actualResults = await autoUIGetListElementsCaptions(term);
+    //console.log("actualResults=" + this.actualResults.length);
+    //console.log("actualResults[0]=" + this.actualResults[0]);
 });
 
-Then('at least one element of the first 10 item of the list should have the caption "Playstation 4"', async function() {
+Then('at least one element of the first 10 item of the list should have the caption {string}', async function(caption) {
     var found = false;
-    for (let i=0; i<this.actualResults.length || i<10 ;i++) {
-        if (this.actualResults[i] === "Playstation 4") {
+    for (var i=0; i<this.actualResults.length || i<10 ;i++) {
+        //console.log("caption=" + caption);
+        //console.log("item=" + this.actualResults[i]);
+        if (this.actualResults[i].indexOf(caption) > -1) {
             found = true;
             break;
         }
